4161c5e2df2c69ba3f66ba36f6b1f6f9eda9eb61
Luis <luisbarbosa.santos@outlook.com>
2025-10-01 20:39:50 -0300

    Component: Init header, define home design and others adjustments

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,20 @@
+export const Header = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-10 flex w-full max-h-[8rem] items-center rounded-md bg-white/85 text-stone-900 backdrop-blur-2xl">
+      <ul className="flex items-center justify-start gap-5 px-10 py-5">
+        <li className="flex cursor-pointer items-center justify-center rounded-lg px-1 py-2 hover:bg-stone-100 hover:text-stone-700">
+          Home
+        </li>
+        <li className="flex cursor-pointer items-center justify-center rounded-lg px-1 py-2 hover:bg-stone-100 hover:text-stone-700">
+          Ajude
+        </li>
+        <li className="flex cursor-pointer items-center justify-center rounded-lg px-1 py-2 hover:bg-stone-100 hover:text-stone-700">
+          Sobre-nós
+        </li>
+        <li className="flex cursor-pointer items-center justify-center rounded-lg px-1 py-2 hover:bg-stone-100 hover:text-stone-700">
+          Contato
+        </li>
+      </ul>
+    </nav>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-
 export default function Home() {
   return (
-    <div>TESTE</div>
-  );
+    <main className="h-screen mt-[2rem] items-center justify-center flex">
+      <img
+        src="https://cdn.pixabay.com/photo/2020/10/05/10/51/cat-5628953_1280.jpg"
+        alt=""
+        className="absolute z-0 w-full h-[90%] opacity-90 object-cover"
+      />
+      <div className="relative z-10 flex-col bg-stone-700/10 rounded-3xl py-3 px-2">
+        <p className="text-8xl font-bold text-stone-100 text-shadow-2xs text-shadow-stone-700">
+          O Poder está em suas Mãos
+        </p>
+      </div>
+    </main>
+  )
 }
