e517b9c49a70ee625822a1762beab7874a37028d
Luis <luisbarbosa.santos@outlook.com>
2025-10-10 10:22:10 -0300

    Add some adjustments

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-5 left-[15%] z-10 flex max-h-[6rem] items-center rounded-4xl inset-shadow-sm bg-stone-50 text-neutral-200 border-2 lg:w-[70%]">
-      <ul className="flex w-full items-center justify-center gap-5 px-10 py-5 font-medium font-AlanSans text-neutral-700 lg:text-[1.35rem]">
+    <nav className="fixed top-5 left-[15%] z-10 flex max-h-[6rem] items-center rounded-4xl inset-shadow-sm bg-neutral-50 text-neutral-200 border-2 lg:w-[70%]">
+      <ul className="flex w-full items-center justify-center gap-5 px-10 py-5 font-medium font-AlanSans text-stone-700 lg:text-[1.35rem]">
         <li className="flex cursor-pointer items-center justify-center rounded-lg px-1 py-2 hover:text-neutral-600">
           Home
         </li>
