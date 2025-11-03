1ae21c36b93a5441117b4565b0c18df307d8de26
vitingr <vitorgabrielsbo1460@gmail.com>
2025-11-03 00:02:12 -0300

    feat(context next-auth-provider): add auth provider

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,26 @@
+import type { User as UserType } from './authentication'
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    accessToken?: string
+    error?: string
+    user: UserType
+  }
+
+  interface User {
+    accessToken?: string
+    expiresIn?: number
+    refreshToken?: string
+  }
+
+  interface JWT {
+    accessToken?: string
+    accessTokenExpires?: number
+    error?: string
+    refreshToken?: string
+  }
+}
+
 declare module '*.css' {
   const content: { [className: string]: string }
   export default content
---
+++ src/types/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  createdAt: string
+  email: string
+  googleId?: string
+  id?: string
+  name: string
+  profilePicture?: string
+  token?: string
+  updatedAt: string
+  userId: string
+}
