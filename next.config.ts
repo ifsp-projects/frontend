5daadd1baf15d148208cdd07519c4c818662d4d2
medinavs <murilorockstar492@gmail.com>
2025-12-04 16:18:20 -0300

    chore: update next.config.ts to comment out unused image domain

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   reactStrictMode: false,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    // domains: ['lh3.googleusercontent.com'],
     remotePatterns: [
       {
         protocol: 'https',
