de8bd3367940316214d8380444cd1fb97ee97351
vitingr <vitorgabrielsbo1460@gmail.com>
2025-12-06 09:59:49 -0300

    fix

--- src/utils/seo/generate-sitemap/static-routes.ts
+++ src/utils/seo/generate-sitemap/static-routes.ts
@@ -8,22 +8,15 @@ import { routeBuilder } from './route-builder'
 export const staticRoutesBuilder = lastModified => {
   const routesArray = []
 
-  console.log('teste')
-
   const processRoutes = (pages: string[]) => {
     for (const indexRoute of Object.keys(pages)) {
-      console.log(indexRoute)
       const route = STATIC_PATHS[indexRoute]
-      console.log(route)
-      console.log(pages[indexRoute]?.sitemap)
       if (route) {
         const formattedRoute = `${BASE_URL}${route}`
         routesArray.push(routeBuilder(formattedRoute, lastModified, '1.0'))
       }
     }
   }
-
-  console.log(STATIC_PATHS)
 
   processRoutes(STATIC_PATHS)
 
