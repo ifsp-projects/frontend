4b1fa196952a513fceddf54c92bb09b7109abb75
unknown <vitorgabrielsbo1460@gmail.com>
2026-03-17 12:17:55 -0300

    fix(pages contato): adjust header

--- src/app/(pages)/contato/sections/header/index.tsx
+++ src/app/(pages)/contato/sections/header/index.tsx
@@ -15,12 +15,12 @@ export const Header: FC = () => {
       </p> */}
       <header className="mx-auto flex w-full max-w-2xl flex-col gap-8 lg:max-w-7xl lg:flex-row lg:items-center lg:gap-16">
         <article className="flex w-full flex-col gap-4">
-          <h1 className="text-left text-2xl font-bold lg:text-4xl xl:text-5xl">
+          <h1 className="text-center text-2xl font-bold lg:text-left lg:text-4xl xl:text-5xl">
             Quer que sua ONG faça parte do projeto? Envie uma solicitação pelo
             formulário abaixo.
           </h1>
 
-          <p className="text-left text-sm text-neutral-500 lg:text-base xl:text-lg">
+          <p className="text-center text-sm text-neutral-500 lg:text-left lg:text-base xl:text-lg">
             Este formulário é para organizações que desejam participar da
             plataforma. Após o envio, nossa equipe irá analisar as informações
             da sua ONG. Se estiver tudo certo, enviaremos um convite no seu
