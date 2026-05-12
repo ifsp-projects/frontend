ecc64140914d4564344cb47c7067c4d7ce27faae
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2026-05-12 11:29:02 -0300

    fix(components navbar): adjust stylwes

--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -151,14 +151,14 @@ export const Navbar = ({ orgs = [] }: NavbarProps) => {
             ) : (
               <div className="flex w-full items-center justify-end gap-3">
                 <Link
-                  className="w-full cursor-pointer rounded-full bg-neutral-700 px-4 py-2 text-center text-sm font-semibold text-white transition-all duration-150 hover:bg-neutral-800 lg:max-w-[115px]"
+                  className="w-full cursor-pointer rounded-full bg-neutral-700 px-4 py-1.5 text-center text-sm font-semibold text-white transition-all duration-150 hover:bg-neutral-800 lg:max-w-[115px]"
                   href="/login"
                   onClick={() => setMenuOpen(false)}
                 >
                   Entrar
                 </Link>
                 <Link
-                  className="w-full max-w-[150px] cursor-pointer rounded-full border border-neutral-700 px-4 py-2 text-center text-sm font-semibold text-neutral-700 transition-all duration-150 hover:bg-neutral-50"
+                  className="w-full max-w-[150px] cursor-pointer rounded-full border border-neutral-700 px-4 py-1.5 text-center text-sm font-semibold text-neutral-700 transition-all duration-150 hover:bg-neutral-50"
                   href="/contato"
                   onClick={() => setMenuOpen(false)}
                 >
